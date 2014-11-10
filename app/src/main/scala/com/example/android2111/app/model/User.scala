418dd22674573cd20672993f7e53be945927e7e9
Raul Bache <basselop@gmail.com>
2014-11-11 00:01:34 +0100

    Renamed imports

--- app/src/main/scala/com/example/android2111/app/model/User.scala
+++ app/src/main/scala/com/example/android2111/app/model/User.scala
@@ -1,14 +1,14 @@
 package com.example.android2111.app.model
 
-import com.j256.ormlite.field.DatabaseField
-import com.j256.ormlite.table.DatabaseTable
+import com.j256.ormlite.field.{ DatabaseField => Field }
+import com.j256.ormlite.table.{ DatabaseTable => Table }
 
-@DatabaseTable class User(pname: String, page: Int) extends WithId { // https://issues.scala-lang.org/browse/SI-8975
-  @DatabaseField val name = pname
-  @DatabaseField val age = page
+@Table class User(pname: String, page: Int) extends WithId { // https://issues.scala-lang.org/browse/SI-8975
+  @Field val name = pname
+  @Field val age = page
   private def this() = this(null, -1)
 }
 
 trait WithId {
-  @DatabaseField(generatedId = true) private var id: Int = _
+  @Field(generatedId = true) private var id: Int = _
 }
