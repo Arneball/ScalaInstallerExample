5359e61bee278be25a0f02f2099cd1a1f3a97ad3
Raul Bache <basselop@gmail.com>
2014-11-27 16:55:50 +0100

    Traversable ViewGroups

--- app/src/main/scala/com/example/android2111/app/package.scala
+++ app/src/main/scala/com/example/android2111/app/package.scala
@@ -1,7 +1,7 @@
 package com.example.android2111
 
 import android.database.Cursor
-import android.view.View
+import android.view.{ViewGroup, View}
 import android.view.View.OnClickListener
 import android.widget.{ImageView, TextView}
 
@@ -15,10 +15,20 @@ package object app {
       def gtTxt(id: Int) = fid(id).asInstanceOf[TextView]
       def gtImg(id: Int) = fid(id).asInstanceOf[ImageView]
       def tag[T] = v.getTag.asInstanceOf[T]
+      def getViews: Traversable[View] = v match {
+        case vg: ViewGroup => vg.flatMap{ _.getViews }
+        case v: View => List(v)
+      }
     }
 
     implicit class TextViewW(val e: TextView) extends AnyVal {
       def text = e.getText.toString
+    }
+
+    implicit class ViewGroupWrapper(val v: ViewGroup) extends Traversable[View] {
+      def foreach[U](f: View => U) = {
+        0 until v.getChildCount foreach (v.getChildAt _ andThen f)
+      }
     }
 
     implicit def str2column(name: String)(implicit cursor: Cursor): Int = cursor.getColumnIndex(name)
